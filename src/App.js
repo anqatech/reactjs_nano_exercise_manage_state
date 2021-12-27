2030bdd9d723d74235be639d650bfebee59cd8c2
anqatech <jalal@anqatech.com>
2021-12-27 08:26:50 +0000

    Finalized the exercise

--- src/App.js
+++ src/App.js
@@ -2,19 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
 
 class App extends Component {
   constructor(props) {
     super(props)
+    const value1 = Math.floor(Math.random() * 100);
+    const value2 = Math.floor(Math.random() * 100);
+    const value3 = Math.floor(Math.random() * 100);
+
     this.state = {
-      date: new Date()
+      value1: value1,
+      value2: value2,
+      value3: value3,
+      proposedAnswer: Math.floor(Math.random() * 3) + value1 + value2 + value3,
+      numQuestions: 0,
+      numCorrect: 0
     }
+  }
+  
+  handleAnswer = (answer) => {
+    this.setState(function(previousState) {
+      const trueAnswer = previousState.value1 + previousState.value2 + previousState.value3  === previousState.proposedAnswer
+      const value1 = Math.floor(Math.random() * 100)
+      const value2 = Math.floor(Math.random() * 100)
+      const value3 = Math.floor(Math.random() * 100)
+      return {
+        value1: value1,
+        value2: value2,
+        value3: value3,
+        proposedAnswer: Math.floor(Math.random() * 3) + value1 + value2 + value3,
+        numQuestions: previousState.numQuestions + 1,
+        numCorrect: (answer === trueAnswer) ? previousState.numCorrect + 1 : previousState.numCorrect        
+      }
+    })
   }
   
   render() {
@@ -27,12 +47,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={() => this.handleAnswer(true) }>True</button>
+          <button onClick={() => this.handleAnswer(false) }>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
